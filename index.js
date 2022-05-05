66ea437693fba6014481bbecb014c5d1a877b385
Bahoz <bahozali50@gmail.com>
2022-05-06 00:50:04 +0300

    project is fnish

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ const quoteParagraph = document.querySelector(".quote_paragraph");
 const genre = document.querySelector(".genre");
 const author = document.querySelector(".author");
 const randomButton = document.querySelector(".random");
+const authorQuoteButton = document.querySelector(".footer__information");
+const quotesMain = document.querySelector(".quotes");
 
 // start quote service start.
 async function randomQuote() {
   try {
     const response = await fetch(`${API_URL}${API_VERSION}quotes/random`);
     const quote = await response.json();
-    updateQuote(quote);
+    renderNewQuote(quote);
   } catch (error) {
     quoteParagraph.innerHTML = "sorry the api not working";
   }
 }
 
-function updateQuote(quote) {
+function renderNewQuote(quote) {
   const { quoteText, quoteAuthor, quoteGenre } = quote.data[0];
 
   quoteParagraph.innerHTML = quoteText;
@@ -26,17 +28,21 @@ function updateQuote(quote) {
   genre.innerHTML = quoteGenre;
 }
 
-const authorQuotes = (authorName, page = 1, limit = 10) => {
-  return new Promise((success, reject) => {
-    if (authorName === undefined) reject({ error: "Author is required" });
+async function authorQuotes(authorName, page = 1, limit = 10) {
+  if (authorName === undefined) return { error: "Author is required" };
 
+  try {
     const endPoint = `${API_URL}${API_VERSION}authors/${authorName}?page=${page}&limit=${limit}`;
-    fetch(endPoint)
-      .then(({ data }) => success(data))
-      .catch((error) => reject(error));
-  });
-};
+    const response = await fetch(endPoint);
+    const quotes = await response.json();
+    return quotes;
+  } catch (error) {
+    return { error: "Api are not working" };
+  }
+}
+// end service
 
+// for now we do'nt use these function ...
 const genreQuotes = (genre, page = 1, limit = 10) => {
   return new Promise((success, reject) => {
     if (genre === undefined) reject({ error: "Genre is required" });
@@ -47,13 +53,17 @@ const genreQuotes = (genre, page = 1, limit = 10) => {
   });
 };
 
-const getQuotes = (page = 1, limit = 10) => {
-  return new Promise((success, reject) => {
-    fetch(`${API_URL}${API_VERSION}quotes?page=${page}&limit=${limit}`)
-      .then(({ data }) => success(data))
-      .catch((error) => reject(error));
-  });
-};
+async function getQuotes(page = 1, limit = 10) {
+  const endPoint = `${API_URL}${API_VERSION}quotes?page=${page}&limit=${limit}`;
+
+  try {
+    const response = await fetch(endPoint);
+    const quotes = await response.json();
+    return quotes;
+  } catch (error) {
+    return { error: "api is not working" };
+  }
+}
 
 const searchQuotes = (searchKeyword, page = 1, limit = 10) => {
   return new Promise((success, reject) => {
@@ -66,7 +76,33 @@ const searchQuotes = (searchKeyword, page = 1, limit = 10) => {
       .catch((error) => reject(error));
   });
 };
-// end service
+// end useless functions
 
 randomQuote();
 randomButton.addEventListener("click", randomQuote);
+
+function templateQuote(quote) {
+  return `<section class="quote-section">
+      <div class="quote__body">
+        <p class="quote_paragraph">${quote}</p>
+      </div>
+    </section>`;
+}
+
+authorQuoteButton.addEventListener("click", setQuotes);
+
+async function setQuotes() {
+  const response = await getQuotes();
+  console.log(response.data[0].quoteText);
+
+  randomButton.style.display = "none"
+  document.getElementById("random-quote").style.display = "none";
+  document.body.style.height = "100%"
+
+  if (!response.error) {
+    response.data.forEach((q) => {
+      const template = templateQuote(q.quoteText);
+      quotesMain.innerHTML += template;
+    });
+  }
+}
